a06dc06687345dff08c3a9de3907189edd27b734
melodyxmw <melodyxmw@gmail.com>
2020-11-01 13:56:18 -0800

    week1

--- React/PropItUp/client/src/components/PersonCard.js
+++ React/PropItUp/client/src/components/PersonCard.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 
 export class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            currentAge:props.age
+        };
+    }
+
+    addAge = () => {
+        this.setState({currentAge: this.state.currentAge +1});
+    }
+
     render() {
         const {firstName, lastName, age, hairColor} = this.props;
         return (
             <div>
                 <h1>{lastName} {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.currentAge}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.addAge}>Birthday Button for {firstName} {lastName}</button>
             </div>
         )
     }
